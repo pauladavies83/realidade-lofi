df82e7fc361faaaf0133e88d166ba6ca36003ce5
Paula <paula.rezende@usp.br>
2022-07-20 12:17:47 -0300

    wt5

--- 07-modelo-world-tracking-stop/main.js
+++ 07-modelo-world-tracking-stop/main.js
@@ -1,5 +1,8 @@
+
 import {loadGLTF} from "../libs/loader.js";
+
 import * as THREE from '../libs/three.js-r132/build/three.module.js';
+
 import {ARButton} from '../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 const normalizeModel = (obj, height) => {
